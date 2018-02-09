78fd5860117ca12adceb2df353398769a1f70951
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-02-09 13:56:29 -0700

    Added a ton of importations, and I added code so the bot can respond to a message of Hello. I also added a eventlistener.

--- src/main/java/discordBot/controller/BotController.java
+++ src/main/java/discordBot/controller/BotController.java
@@ -1,17 +1,49 @@
 package discordBot.controller;
 
+
+
+/**
+ * These are the importations for the JDA and also one for authorizathion.
+ */
+import javax.security.auth.login.LoginException;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.ChannelType;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
-public class BotController extends ListenerAdapter
+/**
+ * This is a class called BotController. It inherits from ListenerAdapter.
+ * @author Jeremy
+ *
+ */
+public class BotController extends ListenerAdapter 
 {
-	JDA tryBot;
+	JDA tryBot; // A JDA called tryBot.
 	
-	public BotController()
+	public BotController() throws LoginException, InterruptedException 
 	{
 		tryBot = new JDABuilder(AccountType.BOT).setToken("NDA0MDM0NjYzNzkxODUzNTY4.DV-OPg.1jMa0nLA_QOusa1m392jdE0uuvs").buildBlocking();
+		// Adding tryBot has a JDABuilder.
+		
+		tryBot.addEventListener(new BotController()); // Adds an EventListener.
+	}
+	
+	@Override
+	public void onMessageReceived(MessageReceivedEvent event)
+	{
+		Message mail = event.getMessage();
+		MessageChannel mailPather = event.getChannel();
+		User member = event.getAuthor();
+		
+		if (mail.getContentRaw().equalsIgnoreCase("Hello"))
+		{
+			mailPather.sendMessage("Hello there, I am Merciz. How may I help you, " + member.getAsMention() + "." ).queue();
+		}
 	}
 	
 	void start()
--- src/main/java/discordBot/controller/BotRunner.java
+++ src/main/java/discordBot/controller/BotRunner.java
@@ -1,8 +1,10 @@
 package discordBot.controller;
 
+import javax.security.auth.login.LoginException;
+
 public class BotRunner 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws LoginException, InterruptedException
     {
         BotController app = new BotController();
         app.start();
