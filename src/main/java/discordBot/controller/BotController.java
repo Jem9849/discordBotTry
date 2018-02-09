a435fa44853894c697e9887f918a6a008797b01e
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-02-09 13:06:39 -0700

    Added a constructor, and added a JDA of tryBot. Also adding some importations.

--- src/main/java/discordBot/controller/BotController.java
+++ src/main/java/discordBot/controller/BotController.java
@@ -1,7 +1,19 @@
 package discordBot.controller;
 
-public class BotController 
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class BotController extends ListenerAdapter
 {
+	JDA tryBot;
+	
+	public BotController()
+	{
+		tryBot = new JDABuilder(AccountType.BOT).setToken("NDA0MDM0NjYzNzkxODUzNTY4.DV-OPg.1jMa0nLA_QOusa1m392jdE0uuvs").buildBlocking();
+	}
+	
 	void start()
 	{
 		
